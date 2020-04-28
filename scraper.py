7e8a62f2263b660bb1ccd36b31eba6f44782030e
PretTy-Fish <PretTy-Fish@users.noreply.github.com>
2020-04-28 20:09:08 +1000

    Fix and Improvement
    Fixed the issue when there is any slash in the file name.
    Improved the Book class so it does not perform unnecessary getting process. Previously it would still get the files if one file exists, which creates unnecessary internet traffic.
    Slightly modified the scrape method so that it checks the file type when there is only one button on the web page. This is only for consistency consideration, where Springer could provide only ePub but no PDF. This does not really happen though.

--- scraper.py
+++ scraper.py
@@ -26,6 +26,7 @@ class Book:
         self.name    = f'{self.title}, {self.edition}'
         self.subject = self.process(subject)
         self.url     = url
+        self.pdf     = None
         self.epub    = None
 
 
@@ -34,10 +35,7 @@ class Book:
 
 
     def fix(self, title):
-        if 'lin' in sys.platform or 'and' in sys.platform:
-            return title.replace('/', '\\')
-        else:
-            return title
+        return title.replace('/', '_')
 
 
     def process(self, subject):
@@ -55,31 +53,46 @@ class Book:
         return subject
 
 
-    def scrape(self):
+    def check(self):
+
+        if os.path.exists(self.path):
+            print(f'Info: {self.path} already saved.')
+            self.save_pdf = None
+
+        if os.path.exists(self.epat):
+            print(f'Info: {self.epat} already saved.')
+            self.save_epub = None
 
         if os.path.exists(self.path) and os.path.exists(self.epat):
-            print(f'Info: {self.path} already saved.')
-            print(f'Info: {self.epat} already saved.')
+            self.scrape = lambda: 0
             self.save = lambda: 0
-            return 0
+
+
+    def scrape(self):
 
         response = requests.get(self.url)
         html  = lxml.html.fromstring(response.content)
+        pdf   = None
         epub  = None
         try:
             xpath = html.xpath(
                 '//*[@id="main-content"]/article[1]/div/div/div[2]/div/div/a'
                 )
             if not bool(xpath):
-                xpath = html.xpath(
+                pdf = html.xpath(
                     '//*[@id="main-content"]/article[1]/div/div/div[2]/div[1]/a'
                     )
+                pdf = pdf[0]
                 epub  = html.xpath(
                     '//*[@id="main-content"]/article[1]/div/div/div[2]/div[2]/a'
                     )
                 epub = epub[0]
-
-            xpath = xpath[0]
+            else:
+                xpath = xpath[0]
+                if 'pdf' in xpath.get('href'):
+                    pdf = xpath
+                else:
+                    epub = xpath
 
         except IndexError:
             print(
@@ -89,12 +102,12 @@ class Book:
             return False
 
         else:
+            if self.save_pdf and pdf:
+                stub  = pdf.get('href')
+                pdf   = f'https://link.springer.com/{stub}'
+                self.pdf  = requests.get(pdf).content
 
-            stub  = xpath.get('href')
-            pdf   = f'https://link.springer.com/{stub}'
-            self.pdf  = requests.get(pdf).content
-
-            if epub:
+            if self.save_epub and epub:
                 stub = epub.get('href')
                 epub = f'https://link.springer.com/{stub}'
                 self.epub = requests.get(epub).content
@@ -102,25 +115,30 @@ class Book:
 
     def save(self):
 
+        if self.save_pdf:
+            self.save_pdf()
+        if self.save_epub:
+            self.save_epub()
 
-        if self.pdf and not os.path.exists(self.path):
+
+    def save_pdf(self):
+
+        if not self.pdf:
+            print(f'Info: Springer does not furnish this as pdf.')
+        elif not os.path.exists(self.path):
             with open(self.path, 'wb') as fhand:
                 fhand.write(self.pdf)
             print(f'Saved: {self.path}')
-        elif not self.pdf:
-            print(f'Info: Springer does not furnish this as pdf.')
-        else:
-            print(f'Info: {self.path} already saved.')
 
 
-        if self.epub and not os.path.exists(self.epat):
+    def save_epub(self):
+
+        if not self.epub:
+            print(f'Info: Springer does not furnish this as epub.')
+        elif self.epub and not os.path.exists(self.epat):
             with open(self.epat, 'wb') as fhand:
                 fhand.write(self.epub)
             print(f'Saved: {self.epat}')
-        elif not self.epub:
-            print(f'Info: Springer does not furnish this as epub.')
-        else:
-            print(f'Info: {self.epat} already saved.')
 
 
 
@@ -131,6 +149,7 @@ for idx, row in df.iterrows():
                 df['Subject Classification'].iloc[idx],
                 df['OpenURL'].iloc[idx])
     print('\n', book)
+    book.check()
     book.scrape()
     book.save()
     print('\n')
